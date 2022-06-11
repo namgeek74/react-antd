9abfab66f17f2190002dc258cb0c79ef7d537cc7
namnd <namnguyennd7428@gmail.com>
2022-06-11 14:36:31 +0700

    typograph expand collapse

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons';
 import Typo from "./components/Typo";
 import TypoText from './components/TypoText';
 import Editable from './components/Editable';
+import Expandable from './components/Expanable';
 
 const App = () => {
   const [date, setDate] = useState(null);
@@ -51,7 +52,7 @@ const App = () => {
       <StarOutlined />
       <StarFilled />
       <StarTwoTone twoToneColor="#eb2f96" /> */}
-      <Editable />
+      <Expandable />
     </>
   );
 };
